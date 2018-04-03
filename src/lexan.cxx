6985230f20767cf9d03740c98c4035a1cc162eae
Michael K <michaelk247@yahoo.de>
2018-04-03 18:42:17 +0200

    completed nextsymbol function

--- src/lexan.cxx
+++ src/lexan.cxx
@@ -2,170 +2,112 @@
 /*************** Scanner *******************************/
 
 
-#ifndef GLOBAL_H 
+#ifndef GLOBAL_H
 #include "global.h"
-#endif 
+#endif
+
+#define NORW   14   		/* Zahl der reservierten Worte */
+#define RWSYMLEN  15		/* Max. Länge reservierter Symbole */
 
 
+int lineno;					/* Zeilennummer */
 
-#define NORW   14   		/* Zahl der reservierten Worte */ 
+int num ;           /* Wert einer int-Konstanten  */
+double realnum; 		/* Wert einer real-Konstanten*/
 
-#define RWSYMLEN  15		/* Max. L�nge reservierter Symbole */ 
-
-
-
-
+char idname[BSIZE];      /* Name eines Bezeichners ; wird vom Parser weiterverwendet */
 
 
-int lineno;					/* Zeilennummer */                            		
-
-int num ;                  	/* Wert einer int-Konstanten  */
-double realnum; 			/* Wert einer real-Konstanten*/ 
-
-char idname[BSIZE];      /* Name eines Bezeichners ;
-							wird vom Parser weiterverwendet */ 
+char actchar;       /* gelesenes Zeichen */
 
 
-char actchar;                /* gelesenes Zeichen */ 
-                      
+/*  Um Bezeichner von reservierten Symbolene unterscheiden zu können,
+wird eine Tabelle reservierte Worte verwendet (restable).
+Die Tabelle enthält Einträge für jedes Schlüsselwort, bestehend aus
+dem Schlüsselwort selbst und dem zugehörigen Tokentyp (Codierung vgl.global.h):
+Bei Erkennen eines möglichen Bezeichners wird zuerst die Tabelle
+der reservierten Symbole durchsucht (lookforres);
+wird ein Schlüsselwort gefunden, liefert lookforres den dem Schlüsselwort
+zugeordneten Tokentyp; sonst 0. Bei Ergebnis 0 liegt dann tatsächlich ein
+Bezeichner vor.
+*/
 
 
+/* Struktur eines Eintrags in der Tabelle reservierter Symbole */
+struct ressw {
+  char ressymbol [RWSYMLEN];			/* Symbol */
+  int token;							/* zugehöriger Tokentyp */
+};
 
-/*  Um Bezeichner von reservierten Symbolene unterscheiden zu k�nnen,
-wird eine Tabelle reservierte Worte verwendet (restable). 
+/* Tabelle reservierter Worte */
+struct ressw restable [] = {
+  {"const", CONST},
+	{"var", VAR},
+	{"procedure", PROCEDURE},
+	{"call", CALL},
+	{"begin", BEGIN},
+	{"end", END},
+	{"if", IF},
+	{"then", THEN},
+	{"else",ELSE},
+	{"while",WHILE},
+	{"do", DO},
+	{"int", INT},
+	{"real", REAL},
+	{"fi", FI}
+};
 
-Die Tabelle enth�lt Eintr�ge f�r jedes Schl�sselwort, bestehend aus 
-dem Schl�sselwort selbst und dem zugeh�rigen Tokentyp (Codierung vgl.global.h):
+/* Suchen nach einem reservierten Symbol */
 
-Bei Erkennen eines m�glichen Bezeichners wird zuerst die Tabelle
-der reservierten Symbole durchsucht (lookforres); 
-wird ein Schl�sselwort gefunden, liefert lookforres den dem Schl�sselwort 
-zugeordneten Tokentyp; sonst 0. Bei Ergebnis 0 liegt dann tats�chlich ein
-Bezeichner vor. 
-*/ 
-
-
-
-
-/* Struktur eines Eintrags in der Tabelle reservierter Symbole */ 
-
-
-struct ressw 
-
-{ char ressymbol [RWSYMLEN];			/* Symbol */             
-  int token;							/* zugeh�riger Tokentyp */ 
-} ;
-
-
-
-/* Tabelle reservierter Worte */ 
-
-
-struct ressw 
-			restable [] =  
-				{	"const", CONST,
-					"var", VAR,
-					"procedure", PROCEDURE,
-					"call", CALL,
-					"begin", BEGIN,	
-					"end", END,
-					"if", IF,		
-					"then", THEN,
-					"else",ELSE,
-					"while",WHILE,
-					"do", DO,
-					"int", INT,
-					"real", REAL,
-					"fi", FI
-				};
-
-
-
-
-
-/* Suchen nach einem reservierten Symbol */ 
-
-int lookforres( char *s)
-
-/* Sucht in Tabelle reservierter Worte nach s und liefert zugeh�rigen Token
+/* Sucht in Tabelle reservierter Worte nach s und liefert zugehörigen Token
    falls gefunden,
-   sonst 0 
-*/ 
-
-{	struct ressw *ptr; 
-
-	for ( ptr = restable; ptr < &restable[NORW]; ptr++ )
-
-		if (strcmp (ptr->ressymbol,s) == 0)
-								/* Symbol gefunden */
-			return (ptr->token);
-	
-		return(0);				/* Symbol nicht gefunden */ 
+   sonst 0
+*/
+int lookforres( char *s) {
+  struct ressw *ptr;
+  for ( ptr = restable; ptr < &restable[NORW]; ptr++ ) {
+    if (strcmp(ptr->ressymbol, s) == 0) {
+			return (ptr->token); /* Symbol gefunden */
+    }
+  }
+	return(0); /* Symbol nicht gefunden */
 }
 
+/******************* Initialisieren des Scanners **********************/
 
-
-
-
-
-/******************* Initialisieren des Scanners **********************/ 
-
-/* wird aufgerufen von  initialize () aus init.cxx ; 
-nimmt Vorbesetzungen der Variablen num, realnum,  idname und lineno vor; 
-
-
-liest das erste Zeichen aus der Eingabe */ 
-
-
-
-void initlexan()
-
-{ num = NONE;						
-  realnum = 0.0; 
-  idname [0] = '\0';					
+/* wird aufgerufen von  initialize () aus init.cxx ;
+nimmt Vorbesetzungen der Variablen num, realnum,  idname und lineno vor;
+liest das erste Zeichen aus der Eingabe */
+void initlexan() {
+  num = NONE;
+  realnum = 0.0;
+  idname [0] = '\0';
   lineno = 1;
-  fin.get(actchar);			/* Erstes Zeichen der Eingabe lesen */  
-    
-}    
+  fin.get(actchar);			/* Erstes Zeichen der Eingabe lesen */
+}
 
-
-
-
-
-
-/*******************************************************************************/
-/********** Funktion nextsymbol ************************************************/ 
-/*******************************************************************************/
-
-
-/**** zentrale Funktion zum Lesen des n�chsten lexikalischen Symbols ***************/ 
-/*****  identifiziert n�chstes lexikalisches Symbol der Eingabe **********/
-
-/***** WICHTIG!!!!!   
-		Bei Aufruf von nextsymbol muss  sich das n�chste Eingabezeichen bereits in actchar befinden
-
-*******/ 
-
-
-/**    liefert Codierung des n�chsten Symbols (token): 
-
-		- Konstante:				token == INTNUM und Wert der Konstanten in Variable num
-									token == REALNUM und Wert in realnum
-		- Bezeichner:				token == ID  und Zeiger auf Name in idname 
-		- Schl�sselwort:			token == Tokentyp des reservierten Symbols nach 
-									Suche in restable 
-		- Operatoren,Sonderzeichen :entsprechende Token 
-
-**/ 
-										
-
-
+/*
+*******************************************************************************
+********** Funktion nextsymbol ************************************************
+*******************************************************************************
+**** zentrale Funktion zum Lesen des nächsten lexikalischen Symbols ***************
+*****  identifiziert nächstes lexikalisches Symbol der Eingabe **********
+***** WICHTIG!!!!!
+		Bei Aufruf von nextsymbol muss  sich das nächste Eingabezeichen bereits in actchar befinden
+*******
+liefert Codierung des nächsten Symbols (token):
+   - Konstante:				   token == INTNUM und Wert der Konstanten in Variable num
+									       token == REALNUM und Wert in realnum
+   - Bezeichner:				 token == ID  und Zeiger auf Name in idname
+	 - Schlüsselwort:			 token == Tokentyp des reservierten Symbols nach Suche in restable
+	 - Operatoren,Sonderzeichen :entsprechende Token
+**/
 
 int nextsymbol () 
 
 {
 	int token;  
-	char lexbuf[BSIZE] ;		/* lokaler Puffer f�r Eingabezeichen */ 	
+	char lexbuf[BSIZE] ;		/* lokaler Puffer für Eingabezeichen */ 	
 
 
 
@@ -173,7 +115,7 @@ int nextsymbol ()
 	{ 
 
 		if ( actchar== ' ' || actchar== '\t')
-			/*Blank und Tab in Ausgabedatei kopieren und �berlesen */ 
+			/*Blank und Tab in Ausgabedatei kopieren und überlesen */ 
 			{	fout.put(actchar);
 				fin.get(actchar);
 				
@@ -181,7 +123,7 @@ int nextsymbol ()
 
 
 		else if (actchar== '\n'  ||  actchar == '\r')  
-			/* Newline in Ausgabedatei kopieren, �berlesen/entfernen, Zeilennummer erh�hen */ 
+			/* Newline in Ausgabedatei kopieren, überlesen/entfernen, Zeilennummer erhöhen */ 
 			{ fout.put(actchar);
 			  fin.get(actchar);
 			  lineno++;
@@ -192,7 +134,7 @@ int nextsymbol ()
 
 				{ /***** actchar ist Ziffer --> Konstanten erkennen  *****/
 					
-					char zahl [BSIZE];		/* Puffer f�r Ziffern */ 
+					char zahl [BSIZE];		/* Puffer für Ziffern */ 
 					int b = 0;                      /* Zeichenzahl*/ 
                                                                                 
                                         zahl[b] = actchar;
@@ -351,7 +293,7 @@ int nextsymbol ()
  	}/* end while */ 
 
 
- 	return(DONE); 	/* EIngabe -Ende erreicht */ 
+ 	return(DONE); 	/* Eingabe -Ende erreicht */ 
 		
 }
 
